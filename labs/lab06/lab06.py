b624bea93b00e498fce62c03c28e28739fa87974
Weenming <20307110161@fudan.edu.cn>
2022-11-04 00:47:38 +0800

    lab 06

---
+++ labs/lab06/lab06.py
@@ -0,0 +1,101 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    def adder_inc(x):
+        nonlocal a
+        res = x + a
+        a += 1
+        return res
+    return adder_inc
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    last_last = -1
+    last = -1
+
+    def fib():
+        nonlocal last_last
+        nonlocal last
+        if last != -1 and last_last != -1:
+            res = last + last_last
+            last_last = last
+            last = res
+            return res
+        elif last == -1:
+            # first call
+            last = 0
+            return 0
+        else:
+            # second call
+            last_last = 0
+            last = 1
+            return 1
+    return fib
+
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    i = 0
+    did_insert = False
+    while (i < len(lst)):
+        if lst[i] == entry:
+            lst.insert(i + 1, elem)
+            i += 1
+            did_insert = True
+        i += 1
+
+    if did_insert:
+        return lst
+    else:
+        return
